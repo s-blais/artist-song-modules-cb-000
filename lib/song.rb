aefb98313dc46abbf1aaeb4ac1a9bd51efcba31a
Stephen Blais <stephen@s-blais.com>
2019-09-04 22:44:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
   attr_reader :artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
+  include Paramable::InstanceMethods
 
   @@songs = []
 
@@ -20,7 +21,7 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
