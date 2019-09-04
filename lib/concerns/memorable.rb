10fd58a693bb115971da89dfc2014b8db5e766f5
Stephen Blais <stephen@s-blais.com>
2019-09-04 22:28:33 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,12 +2,12 @@ module Memorable
 
   module ClassMethods
 
-    def self.reset_all
-      all.clear
+    def reset_all
+      self.all.clear
     end
 
-    def self.count
-      all.count
+    def count
+      self.all.count
     end
 
   end
