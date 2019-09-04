8f799dc160cf8ef45973e15d97d63e6f1938734e
Stephen Blais <stephen@s-blais.com>
2019-09-04 22:58:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,6 @@ class Song
 
   @@songs = []
 
-  # def initialize
-  #   @@songs << self
-  # end
-
   def self.all
     @@songs
   end
@@ -22,7 +18,4 @@ class Song
     @artist = artist
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
